b260449f178f78bf3e54224782cb9d6aac1c23ad
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2021-01-27 15:44:47 +0100

    Første små endringer.

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -4,44 +4,56 @@ import java.util.Arrays;
 
 import no.hvl.dat110.TODO;
 
-public class Message {
+public class Message
+{
 
 	private byte[] payload;
 
-	public Message(byte[] payload) {
+	public Message(byte[] payload)
+	{
 		this.payload = payload; // TODO: check for length within boundary
 	}
 
-	public Message() {
+	public Message()
+	{
 		super();
 	}
 
-	public byte[] getData() {
-		return this.payload; 
+	public byte[] getData()
+	{
+		return this.payload;
 	}
 
-	public byte[] encapsulate() {
-		
-		byte[] encoded = null;
-		
+	public byte[] encapsulate()
+	{
+
 		// TODO
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
 		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
-
-		return encoded;
+		byte[] encoded = null;
+		int length = payload.length;
 		
+		return encoded;
+
 	}
 
-	public void decapsulate(byte[] received) {
+	public void decapsulate(byte[] received)
+	{
 
 		// TODO
-		// decapsulate the data contained in the received byte array and store it 
+		// decapsulate the data contained in the received byte array and store it
 		// in the payload of this message
 		
-		throw new UnsupportedOperationException(TODO.method());
+		int length = received[0];
+		byte[] payload = new byte[length];
 		
+		for (int i = 1; i <= length; i++)
+		{
+			payload[i-1] = received[i];
+		}
+
+		throw new UnsupportedOperationException(TODO.method());
+
 	}
 }
